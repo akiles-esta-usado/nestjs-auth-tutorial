f1aabc3389f51ba5d8f0af6d61de043fd5dda2db
aquiles viza <aquiles.viza@sansano.usm.cl>
2025-02-05 20:59:46 -0400

    Removed unused configuration from AuthV2

--- src/auth-v2/auth-v2.module.ts
+++ src/auth-v2/auth-v2.module.ts
@@ -5,19 +5,10 @@ import { UsersModule } from '../users/users.module.ts';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy.ts';
 import { JwtStrategy } from './strategies/jwt.strategy.ts';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants.ts';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      // https://github.com/nestjs/jwt/blob/master/README.md
-      // https://github.com/auth0/node-jsonwebtoken#usage
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '30s' },
-    }),
     PassportModule,
   ],
   controllers: [PassportAuthController],
